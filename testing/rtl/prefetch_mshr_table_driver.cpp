c30d94cdbfadd779e1038a3353ea2e382a840682
Connor Yang <connoryang2005@gmail.com>
2026-09-11 14:18:25 -0400

    Test demand joins at nonzero line offset

--- testing/rtl/prefetch_mshr_table_driver.cpp
+++ testing/rtl/prefetch_mshr_table_driver.cpp
@@ -22,7 +22,7 @@ int main() {
   d.issue_id_i = 0; d.issue_v_i = 1; d.eval(); assert(d.issue_ready_o); tick(d, 0); d.issue_v_i = 0;
   d.issue_id_i = 1; d.issue_v_i = 1; d.eval(); assert(d.issue_ready_o); tick(d, 0); d.issue_v_i = 0;
   assert(d.valid_o == 3 && d.issued_o == 3 && d.addr0_o == 0x1000 && d.addr1_o == 0x2000);
-  d.demand_addr_i = 0x2000; d.demand_v_i = 1; d.eval();
+  d.demand_addr_i = 0x2038; d.demand_v_i = 1; d.eval();
   assert(d.demand_join_o && d.demand_id_o == 1); d.demand_v_i = 0;
   d.response_id_i = 1; d.response_beat_i = 2; d.response_v_i = 1; d.response_last_i = 0; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = 0;
   assert(d.valid_o == 3 && d.issued_o == 3 && d.fill_mask1_o == 4);
