f0d7a14af43bceb8553c9340909fb89aaf15e779
Connor Yang <connoryang2005@gmail.com>
2026-09-11 14:17:44 -0400

    Verify partial prefetch fill progress

--- testing/rtl/prefetch_mshr_table_driver.cpp
+++ testing/rtl/prefetch_mshr_table_driver.cpp
@@ -14,7 +14,7 @@ static void reset(Vprefetch_mshr_table_tb &d) {
 int main() {
   Vprefetch_mshr_table_tb d;
   d.alloc_v_i = d.issue_v_i = d.response_v_i = d.response_last_i = d.demand_v_i = 0;
-  d.issue_id_i = d.response_id_i = 0; reset(d);
+  d.issue_id_i = d.response_id_i = d.response_beat_i = 0; reset(d);
   d.alloc_addr_i = 0x1000; d.alloc_context_i = 1; d.alloc_way_i = 3; d.alloc_v_i = 1;
   d.eval(); assert(d.alloc_yumi_o && d.alloc_id_o == 0); tick(d, 0); d.alloc_v_i = 0;
   d.alloc_addr_i = 0x2000; d.alloc_context_i = 2; d.alloc_way_i = 5; d.alloc_v_i = 1;
@@ -24,7 +24,9 @@ int main() {
   assert(d.valid_o == 3 && d.issued_o == 3 && d.addr0_o == 0x1000 && d.addr1_o == 0x2000);
   d.demand_addr_i = 0x2000; d.demand_v_i = 1; d.eval();
   assert(d.demand_join_o && d.demand_id_o == 1); d.demand_v_i = 0;
-  d.response_id_i = 1; d.response_v_i = d.response_last_i = 1; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = d.response_last_i = 0;
+  d.response_id_i = 1; d.response_beat_i = 2; d.response_v_i = 1; d.response_last_i = 0; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = 0;
+  assert(d.valid_o == 3 && d.issued_o == 3 && d.fill_mask1_o == 4);
+  d.response_v_i = d.response_last_i = 1; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = d.response_last_i = 0;
   assert(d.valid_o == 1 && d.issued_o == 1);
   d.response_id_i = 0; d.response_v_i = d.response_last_i = 1; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = d.response_last_i = 0;
   assert(d.valid_o == 0 && d.issued_o == 0);
