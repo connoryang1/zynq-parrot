12a19901bc2de98b13657bc33919e3342bc9ce15
Connor Yang <connoryang2005@gmail.com>
2026-09-11 14:13:55 -0400

    Test demand joins for prefetch entries

--- testing/rtl/prefetch_mshr_table_driver.cpp
+++ testing/rtl/prefetch_mshr_table_driver.cpp
@@ -13,7 +13,7 @@ static void reset(Vprefetch_mshr_table_tb &d) {
 }
 int main() {
   Vprefetch_mshr_table_tb d;
-  d.alloc_v_i = d.issue_v_i = d.response_v_i = d.response_last_i = 0;
+  d.alloc_v_i = d.issue_v_i = d.response_v_i = d.response_last_i = d.demand_v_i = 0;
   d.issue_id_i = d.response_id_i = 0; reset(d);
   d.alloc_addr_i = 0x1000; d.alloc_context_i = 1; d.alloc_way_i = 3; d.alloc_v_i = 1;
   d.eval(); assert(d.alloc_yumi_o && d.alloc_id_o == 0); tick(d, 0); d.alloc_v_i = 0;
@@ -22,6 +22,8 @@ int main() {
   d.issue_id_i = 0; d.issue_v_i = 1; d.eval(); assert(d.issue_ready_o); tick(d, 0); d.issue_v_i = 0;
   d.issue_id_i = 1; d.issue_v_i = 1; d.eval(); assert(d.issue_ready_o); tick(d, 0); d.issue_v_i = 0;
   assert(d.valid_o == 3 && d.issued_o == 3 && d.addr0_o == 0x1000 && d.addr1_o == 0x2000);
+  d.demand_addr_i = 0x2000; d.demand_v_i = 1; d.eval();
+  assert(d.demand_join_o && d.demand_id_o == 1); d.demand_v_i = 0;
   d.response_id_i = 1; d.response_v_i = d.response_last_i = 1; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = d.response_last_i = 0;
   assert(d.valid_o == 1 && d.issued_o == 1);
   d.response_id_i = 0; d.response_v_i = d.response_last_i = 1; d.eval(); assert(d.response_ready_o); tick(d, 0); d.response_v_i = d.response_last_i = 0;
--- testing/rtl/prefetch_mshr_table_tb.sv
+++ testing/rtl/prefetch_mshr_table_tb.sv
@@ -1,8 +1,9 @@
 module prefetch_mshr_table_tb
   (input clk_i, reset_i, alloc_v_i, issue_v_i, response_v_i, response_last_i,
    input [55:0] alloc_addr_i, input [1:0] alloc_context_i, input [2:0] alloc_way_i,
-   input [1:0] issue_id_i, response_id_i,
+   input [1:0] issue_id_i, response_id_i, input demand_v_i, input [55:0] demand_addr_i,
    output alloc_ready_o, alloc_yumi_o, issue_ready_o, response_ready_o,
+   output demand_join_o, output [1:0] demand_id_o,
    output [1:0] alloc_id_o, output [1:0] valid_o, output [1:0] issued_o,
    output [55:0] addr0_o, addr1_o, output [1:0] context0_o, context1_o,
    output [2:0] way0_o, way1_o);
@@ -15,7 +16,8 @@ module prefetch_mshr_table_tb
     (.clk_i, .reset_i, .alloc_ready_o, .alloc_v_i, .alloc_addr_i,
      .alloc_context_i, .alloc_way_i, .alloc_id_o, .alloc_yumi_o,
      .issue_v_i, .issue_id_i, .issue_ready_o, .response_v_i, .response_id_i,
-     .response_ready_o, .response_last_i, .valid_o(valid), .issued_o(issued),
+     .response_ready_o, .response_last_i, .demand_v_i, .demand_addr_i,
+     .demand_join_o, .demand_id_o, .valid_o(valid), .issued_o(issued),
      .addr_o(addr), .context_o(ctx), .way_o(way));
   assign valid_o = valid; assign issued_o = issued;
   assign addr0_o = addr[0]; assign addr1_o = addr[1];
